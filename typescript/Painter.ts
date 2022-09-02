83cd2289137456627a289b4c9e3a77afcf9d8add
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-02 16:38:57 +0300

    If the first event has held=true, that's a bug, so skip it.

--- typescript/Painter.ts
+++ typescript/Painter.ts
@@ -4,24 +4,31 @@ type LineFunction = (x0: number, y0: number, x1: number, y1: number) => void
 
 /** Painting pointer class */
 export class Painter extends Pointer {
-    paintLine: LineFunction
+    readonly paintLine: LineFunction
+    receivedFirstEvent: boolean
 
     constructor(canvas: HTMLCanvasElement, paintLine: LineFunction) {
         super(canvas)
 
         this.paintLine = paintLine
+        this.receivedFirstEvent = false
     }
 
     override setPosition(event: MouseEvent | Touch) {
         const _x = this.x
         const _y = this.y
-        const _held = this.held
 
         super.setPosition(event)
 
-        // Paint by dragging
-        if (_held && this.held) {
-            this.paintLine(_x, _y, this.x, this.y)
+        if (this.receivedFirstEvent) {
+            // Paint by dragging
+            if (this.held) {
+                this.paintLine(_x, _y, this.x, this.y)
+            }
+        }
+        else {
+            // If the first event has held=true, that's a bug, so skip it.
+            this.receivedFirstEvent = true
         }
     }
 }
