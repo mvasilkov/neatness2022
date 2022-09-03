e2422c109f41ea9d02a2b14d71c2b3b58f5d1cac
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-03 22:14:08 +0300

    The Neatness

--- typescript/levels/Neatness02.ts
+++ typescript/levels/Neatness02.ts
@@ -1,5 +1,8 @@
 import { tiles, width, xs, ys } from '../bitmaps/Neatness02.js'
+import { conUI, paintTextBlob } from '../canvas.js'
+import { Colors } from '../colors/colors.js'
 import { Level } from '../Level.js'
+import { Settings } from '../prelude.js'
 
 export class Neatness02 extends Level {
     constructor() {
@@ -7,6 +10,14 @@ export class Neatness02 extends Level {
 
         this.addHotspot(xs[0], ys[0], false)
         this.addHotspot(xs[1], ys[1], true)
+    }
+
+    // @ts-expect-error 't' is declared but its value is never read.
+    override paint(t: number): void {
+        paintTextBlob(conUI,
+            0.5 * Settings.SCREEN_WIDTH,
+            0.15 * Settings.SCREEN_HEIGHT,
+            48, 'bold 48', Colors.tile, 'The Neatness')
     }
 
     override paintInternal() {
