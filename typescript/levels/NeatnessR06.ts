49eb48b61f7c1ecc3a840444b81b59b0c6a1eee6
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-05 23:09:13 +0300

    Update

--- typescript/levels/NeatnessR06.ts
+++ typescript/levels/NeatnessR06.ts
@@ -10,8 +10,8 @@ export class NeatnessR06 extends Level {
         this.addHotspot(xs[0], ys[0], false)
         this.addHotspot(xs[1], ys[1], true)
         this.addHotspot(xs[2], ys[2], false)
-        this.addHotspot(xs[3], ys[3], true)
-        this.addHotspot(xs[4], ys[4], false)
+        this.addHotspot(xs[3], ys[3], false)
+        this.addHotspot(xs[4], ys[4], true)
         this.addHotspot(xs[5], ys[5], true)
     }
 
