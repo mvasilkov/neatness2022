3b1bfcf85d196ec7038b1e51229e2189c1037877
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-05 08:48:00 +0300

    Update

--- typescript/levels/NeatnessR03.ts
+++ typescript/levels/NeatnessR03.ts
@@ -1,5 +1,6 @@
 import { tiles, width, xs, ys } from '../bitmaps/NeatnessR03.js'
 import { Level } from '../Level.js'
+import { Settings } from '../prelude.js'
 
 export class NeatnessR03 extends Level {
     constructor() {
@@ -12,6 +13,6 @@ export class NeatnessR03 extends Level {
     }
 
     override paintInternal() {
-        this.paintInternalTiles(tiles, width)
+        this.paintInternalTiles(tiles, width, 2 * Settings.TILE_WIDTH, 2 * Settings.TILE_HEIGHT)
     }
 }
