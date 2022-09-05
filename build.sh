4266bc23a87a7718bbe015a91356fb1fa9b4187c
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-05 13:27:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,20 @@ node_modules/.bin/rollup -f iife -o build/app.js out/app.js
 
 # Optimize
 node_modules/.bin/terser -cmo build/app.opt.js build/app.js
+node_modules/.bin/cleancss -O1 -o build/app.opt.css out/app.css
+
+cat <<END > build/config.json
+{
+  "collapseWhitespace": true,
+  "removeAttributeQuotes": true,
+  "removeComments": true
+}
+END
+node_modules/.bin/html-minifier-terser -c build/config.json -o build/index.html out/index.html
 
 # Package
-zip -j9 build/app.zip build/app.opt.js
+mv build/app.opt.js build/app.js
+mv build/app.opt.css build/app.css
+zip -j9 build/app.zip build/app.js build/app.css build/index.html
 # brew install advancecomp
 advzip -z4 build/app.zip
